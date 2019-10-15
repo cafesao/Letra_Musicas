56c006460372721901f9d0318972bd58ea316301
cafesao <gabriel.diasdutra2@gmail.com>
2019-10-15 15:44:56 -0300

    [CS] Adicionando JS principal

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,59 @@
-import {procurarMusica, apagarResultadoDiv, apagarResultadoInput} from './funcoes_Pesquisa'
-import { prepararObjeto, apagarInput } from './funcoes_Adicionar'
+//Importando modulos
+import { procurarMusica } from './funcoes_Pesquisa'
+import { prepararObjeto } from './funcoes_Adicionar'
+
+//Variaveis
+var nomeMusicaProcurar = document.querySelector('input#nomeMusicaProcurarInput')
+
+nomeMusicaProcurar.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) { 
+        Pesquisar()
+    }
+})
 
 let botao_Pesquisar = document.querySelector('button#pesquisar') 
 botao_Pesquisar.addEventListener("click", Pesquisar)
 
-let botao_Adicionar = document.querySelector('button#adicionar')
-botao_Adicionar.addEventListener("click", Adicionar)
+//Funções
+function apagarResultadoPesquisa() {
+    nomeMusica.value = ''
+}
+
 
 function Pesquisar() {
-    var divResultado = document.querySelector('div#resultado_buscar')
-    var nomeMusica = document.querySelector('div#buscar input#nomeMusicaProcurarInput')
-
-    if(nomeMusica.value.length === 0) {
+    if(nomeMusicaProcurar.value.length === 0) {
         alert('Por favor digite o nome da musica!')
     }
     else{
-        apagarResultadoDiv(divResultado)
-        procurarMusica(divResultado, nomeMusica.value)    
-        apagarResultadoInput(nomeMusica)
+        procurarMusica(nomeMusicaProcurar.value)    
+        apagarResultadoPesquisa()
     }
 }
 
-function Adicionar() {
-    var divResultado = document.querySelector('div#resultado_adicionar')
-    var nomeMusica = document.querySelector('div#adicionar input#nomeMusicaInput')
-    var nomeArtista = document.querySelector('div#adicionar input#nomeArtistaInput')
-    var lancamento = document.querySelector('div#adicionar input#anoLancamentoInput')
-    var letra = document.querySelector('#letraInput')  
 
+//Variaveis
+var nomeMusica = document.querySelector('input#nomeMusicaInput')
+var nomeArtista = document.querySelector('input#nomeArtistaInput')
+var lancamento = document.querySelector('input#anoLancamentoInput')
+var letra = document.querySelector('#letraInput')  
+
+let botao_Adicionar = document.querySelector('button#adicionar')
+botao_Adicionar.addEventListener("click", Adicionar)
+
+//Funções
+function apagarResultadoAdicionar() {
+    nomeMusica.value = ''
+    nomeArtista.value = ''
+    lancamento.value = ''
+    letra.value = ''
+}
+
+function Adicionar() {
     if(nomeMusica.value.length === 0 || nomeArtista.value.length === 0 || lancamento.value.length === 0 || letra.value.length === 0){
         alert('Por favor, prencha todos os campos!')
     }
     else {
-        prepararObjeto(nomeMusica.value, nomeArtista.value, lancamento.value, letra.value, divResultado)
-        apagarInput(nomeMusica, nomeArtista, lancamento, letra)
+        prepararObjeto(nomeMusica.value, nomeArtista.value, lancamento.value, letra.value)
+        apagarResultadoAdicionar()
     }
 }
